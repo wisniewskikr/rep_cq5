492888281efb0434c4bb7ed7f5f78cdda5ac3d68
Chris <wisniewskik@op.pl>
2013-03-27 20:27:49 +0100

    Add sq5 changes

--- Cq5-helloWorld-09-slingRequest/src/main/java/pl/kwi/TestService.java
+++ Cq5-helloWorld-09-slingRequest/src/main/java/pl/kwi/TestService.java
@@ -8,8 +8,12 @@ public class TestService implements ITestService {
 	public String getHelloWorldMessage(SlingHttpServletRequest slingRequest) {
 		
 		Resource resource = slingRequest.getResource();
+		
 		String name = resource.getName();
 		System.out.println(name);
+		
+		String resourceType = resource.getResourceType();
+		System.out.println(resourceType);
 		
 		return "Hello World from class TestService";
 	}
--- Cq5-helloWorld-09-slingRequest/src/main/webapp/jcr_root/apps/webapp/components/testComponent/testComponent.jsp
+++ Cq5-helloWorld-09-slingRequest/src/main/webapp/jcr_root/apps/webapp/components/testComponent/testComponent.jsp
@@ -1,5 +1,6 @@
 <%@include file="/libs/foundation/global.jsp" %>
 <%@page import="pl.kwi.TestService"%>
 
+
 <div><%= new TestService().getHelloWorldMessage(slingRequest) %></div>
 
