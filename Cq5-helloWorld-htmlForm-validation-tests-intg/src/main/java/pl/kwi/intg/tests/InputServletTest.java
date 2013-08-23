a1d7e406221d000e1aa28bbcc252e80c86bd47c9
unknown <wisniewk@POZMWCOE27631.emea.roche.com>
2013-08-23 15:24:28 +0200

    Add project with intg tests

---
+++ Cq5-helloWorld-htmlForm-validation-tests-intg/src/main/java/pl/kwi/intg/tests/InputServletTest.java
@@ -0,0 +1,14 @@
+package pl.kwi.intg.tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class InputServletTest {
+
+	@Test
+	public void test() {
+		assertTrue(true);
+	}
+
+}
---
+++ Cq5-helloWorld-htmlForm-validation-tests-intg/src/test/java/pl/kwi/intg/main/IntgTest.java
@@ -0,0 +1,24 @@
+package pl.kwi.intg.main;
+
+import org.apache.sling.junit.remote.testrunner.SlingRemoteTestParameters;
+import org.apache.sling.junit.remote.testrunner.SlingRemoteTestRunner;
+import org.junit.runner.RunWith;
+
+//@RunWith(SlingRemoteTestRunner.class)
+public class IntgTest implements SlingRemoteTestParameters {
+
+
+	public String getJunitServletUrl() {
+		return "http://localhost:4502/system/sling/junit";
+	}
+
+	public String getTestClassesSelector() {
+		return "";
+	}
+
+	public String getTestMethodSelector() {
+		return "";
+	}
+	
+
+}
